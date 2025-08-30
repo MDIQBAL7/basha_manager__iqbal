fd6545748f39891d1bde88e739826836c5246eff
MDIQBAL7 <iqbalhossainbn1@gmail.com>
2025-08-30 10:00:50 +0600

    provlem-solved

--- assets/js/my-js.js
+++ assets/js/my-js.js
@@ -23,3 +23,29 @@
           },
         });
       }
+
+
+
+    //   view details images animation 
+
+          const modal = document.getElementById("imageModal");
+          const modalImg = document.getElementById("modalImage");
+          const captionText = document.getElementById("caption");
+          const closeBtn = document.getElementById("closeModal");
+
+          document.querySelectorAll(".gallery-img").forEach((img) => {
+            img.onclick = function () {
+              modal.style.display = "block";
+              modalImg.src = this.src;
+              captionText.innerHTML = this.alt;
+              modalImg.style.animation = "zoomIn 0.5s ease-in-out";
+            };
+          });
+
+          closeBtn.onclick = function () {
+            modalImg.style.animation = "zoomOut 0.5s ease-in-out";
+            setTimeout(() => {
+              modal.style.display = "none";
+            }, 500);
+          };
+     
